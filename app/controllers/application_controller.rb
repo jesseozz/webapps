6bc1643ad4c74a1d2ecc31ac491bb323b8eaa833
jesse ozdamar <jesse.ozdamar@kellogg.northwestern.edu>
2024-02-06 16:13:03 +0000

    push

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+  def index
+    # render :inline => "<h1>I love carnitas!!</h1> Carnitas is slow-cooked pork you uncultured swine"
+    render :template => "tacos/index"
+  end
+
 end
